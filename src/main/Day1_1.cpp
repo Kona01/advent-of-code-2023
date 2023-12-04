2caec6b8fc096828e09cd44446b77b7dd3f69f59
Matthew Jacobs <matthewjacobs@hotmail.ca>
2023-12-04 15:02:04 -0800

    Fixed formatting errors

--- src/main/Day1_1.cpp
+++ src/main/Day1_1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+using namespace std;
 int main() {
     fstream input;
 
@@ -16,22 +16,23 @@ int main() {
         }
     }
 
-    cout << total
+    cout << total;
 }
 
 int readline(string line) {
     int length = line.length();
     int result = 0;
     for (int i = 0; i < length; i++) {
-        if (isdigit(s.at(i))) {
-            result = s.at(i) * 10;
+        if (isdigit(line.at(i))) {
+            result = line.at(i) * 10;
             break;
         }
     }
     for (int i = length - 1; i >= 0; i--) {
-        if (isdigit(s.at(i))) {
-            result += s.at(i);
+        if (isdigit(line.at(i))) {
+            result += line.at(i);
             break;
         }
     }
+    return result;
 }
